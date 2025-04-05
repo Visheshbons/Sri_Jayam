b5c81ba9a173ba19f73ef35efdd2ae2e63e95334
Vishesh Kudva <vishesh.kudva@outlook.com>
2025-04-05 17:18:06 +1300

    Added some initial code.

---
+++ index.js
@@ -0,0 +1,44 @@
+// Starts a timer to see how long the initialisation takes
+console.time("Loading time");
+
+// Imports all the neccecary data
+import chalk from 'chalk';
+import cookieParser from 'cookie-parser';
+
+// Quickly sets up cookies and static files
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// Redirect to "/"
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+// Listen to port
+app.listen(port, () => {
+    // log server start
+    console.log(`Server running on port ` + chalk.green(port) + `.`);
+    console.log(``);
+
+    // log date and time
+    console.log(`Time of start: ${getDateAndTime()}`);
+    console.log(``);
+
+    // log website info
+    console.log(`Go to ` + chalk.dim(`http://localhost:${port}`) + ` to view the website.`);
+    if (portForward) {
+        console.log(`Go to ${chalk.dim(`https://p9npwlmh-${port}.aue.devtunnels.ms/`)} to view the website.`);
+    };
+    console.log(``);
+
+    // log server commands
+    console.log(`Type "rs" to restart the server.`);
+    console.log(`Press CTRL + C to kill the server.`);
+    console.log(``);
+
+    // log info usage log details
+    console.log(`The page loadings will be logged underneath.`)
+    console.log(``);
+    console.log(``);
+});
